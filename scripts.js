c4c10890fa42bb5fd4f9ff81252fc3814f5311c7
khaled1192 <k2555066@gmail.com>
2024-12-04 20:07:36 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -31,3 +31,8 @@ const toggleButton = document.querySelector('.dark-mode-toggle');
 toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode'); // التبديل بين الوضعين
 });
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('open');
+}
